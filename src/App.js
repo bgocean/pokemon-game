93a4a0f5c6284b1826481888a3308b2d446d3b9e
bgocean <51014032+bgocean@users.noreply.github.com>
2022-01-21 19:42:34 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import HeaderBlock from "./components/HeaderBlock";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
+    <>
+      <HeaderBlock title="This is new Title" descr="This is new description!" />
+      <HeaderBlock title="This is new Title" hideBackground />
+    </>
   );
-}
+};
 
 export default App;
